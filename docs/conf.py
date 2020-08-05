8042cd3c13d2895a524a2745abdc005165817717
Alexey Radkov <alexey.radkov@gmail.com>
2020-08-05 10:35:55 +0300

    more settings in conf.py [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,9 @@
+project = u'nginx-haskell-module'
+copyright = u'2016-2020, Alexey Radkov'
+
+html_title = "Nginx Haskell module (yet another doc with examples)"
+html_short_title = "Nginx Haskell module"
+
 master_doc = 'index'
 
 pygments_style = 'friendly'
