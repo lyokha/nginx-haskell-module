13f8ca9faeb6859081d0b3f66626341e2f82a066
Alexey Radkov <alexey.radkov@gmail.com>
2020-08-06 14:05:55 +0300

    updated document structure [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -2,8 +2,7 @@ project = u'nginx-haskell-module'
 author = u'Alexey Radkov'
 copyright = u'2016-2020, Alexey Radkov'
 
-html_title = "Nginx Haskell module (yet another doc with examples)"
-html_short_title = "Nginx Haskell module"
+html_title = "Nginx Haskell module"
 
 master_doc = 'index'
 
--- docs/index.rst
+++ docs/index.rst
@@ -4,4 +4,4 @@ Nginx Haskell module
 .. toctree::
    :maxdepth: 3
 
-   Yet another doc with examples <yet-another-doc-with-examples/nginx-haskell-module-yadwe.rst>
+   yet-another-doc-with-examples/nginx-haskell-module-yadwe.rst
