6cf4cd6a482895228db59df9207d92c239882f4d
lyokha <alexey.radkov@gmail.com>
2022-01-04 15:10:06 +0300

    docs updated [ci skip]

--- haskell/ngx-export-distribution/NgxExport/Distribution.hs
+++ haskell/ngx-export-distribution/NgxExport/Distribution.hs
@@ -299,7 +299,8 @@ buildSharedLib verbosity desc lbi flags = do
 -- - Collects all dependent Haskell libraries in a directory whose name forms
 --   as /arch-os-compiler/ (or that overridden in option /--hslibdeps-options/),
 -- - adds the value of /prefix/ (or that overridden in option
---   /--hslibdeps-options/) to the list of /rpath/ in the shared library,
+--   /--hslibdeps-options/) in the beginning of the list of /rpath/ in the
+--   shared library,
 -- - archives the shared library and the directory with the collected dependent
 --   libraries in a /tar.gz/ file.
 --
