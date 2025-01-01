5cf4283f03f210872e9a0e354d7dd8ace9032dd3
Alexey Radkov <alexey.radkov@gmail.com>
2025-01-01 13:20:53 +0400

    bump year and version

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,8 @@
 project = u'nginx-haskell-module'
 author = u'Alexey Radkov'
-copyright = u'2015-2024, Alexey Radkov'
+copyright = u'2015-2025, Alexey Radkov'
 
-version = '3.2.0'
+version = '3.4.3'
 
 html_title = u'Nginx Haskell module'
 html_logo = 'images/nginx-haskell-module-120.png'
