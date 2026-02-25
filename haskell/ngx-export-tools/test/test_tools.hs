1a916891620e0d34e744504628daf6c23f4a9a5f
Alexey Radkov <alexey.radkov@gmail.com>
2026-02-25 16:20:03 +0400

    use type synonym LazyByteString

--- haskell/ngx-export-tools/test/test_tools.hs
+++ haskell/ngx-export-tools/test/test_tools.hs
@@ -7,7 +7,7 @@ import           NgxExport
 import           NgxExport.Tools
 
 import           Data.ByteString (ByteString)
-import qualified Data.ByteString.Lazy as L
+import           Data.ByteString.Lazy (LazyByteString)
 import qualified Data.ByteString.Lazy.Char8 as C8L
 import           Data.Aeson
 import           Data.IORef
@@ -15,35 +15,35 @@ import           Control.Monad
 import           GHC.Generics
 import           System.Environment
 
-test :: ByteString -> Bool -> IO L.ByteString
-test = const . return . L.fromStrict
+test :: ByteString -> Bool -> IO LazyByteString
+test = const . return . C8L.fromStrict
 ngxExportSimpleService 'test $
     PersistentService $ Just $ Sec 10
 
-showAsLazyByteString :: Show a => a -> L.ByteString
+showAsLazyByteString :: Show a => a -> LazyByteString
 showAsLazyByteString = C8L.pack . show
 
-testRead :: Show a => a -> IO L.ByteString
+testRead :: Show a => a -> IO LazyByteString
 testRead = return . showAsLazyByteString
 
-testReadInt :: Int -> Bool -> IO L.ByteString
+testReadInt :: Int -> Bool -> IO LazyByteString
 testReadInt = const . testRead
 ngxExportSimpleServiceTyped 'testReadInt ''Int $
     PersistentService $ Just $ Sec 10
 
 newtype Conf = Conf Int deriving (Read, Show)
 
-testReadConf :: Conf -> Bool -> IO L.ByteString
+testReadConf :: Conf -> Bool -> IO LazyByteString
 testReadConf = const . testRead
 ngxExportSimpleServiceTyped 'testReadConf ''Conf $
     PersistentService $ Just $ Sec 10
 
-testConfStorage :: ByteString -> IO L.ByteString
+testConfStorage :: ByteString -> IO LazyByteString
 testConfStorage = const $
     showAsLazyByteString <$> readIORef storage_Conf_testReadConf
 ngxExportIOYY 'testConfStorage
 
-testReadConfNoStore :: Conf -> Bool -> IO L.ByteString
+testReadConfNoStore :: Conf -> Bool -> IO LazyByteString
 testReadConfNoStore = testReadConf
 ngxExportSimpleServiceTyped' 'testReadConfNoStore ''Conf $
     PersistentService $ Just $ Sec 10
@@ -52,7 +52,7 @@ data ConfWithDelay = ConfWithDelay { delay :: TimeInterval
                                    , value :: Int
                                    } deriving (Read, Show)
 
-testReadConfWithDelay :: ConfWithDelay -> Bool -> IO L.ByteString
+testReadConfWithDelay :: ConfWithDelay -> Bool -> IO LazyByteString
 testReadConfWithDelay c@ConfWithDelay {..} fstRun = do
     unless fstRun $ threadDelaySec $ toSec delay
     testRead c
@@ -63,42 +63,42 @@ data ConfJSON = ConfJSONCon1 Int
               | ConfJSONCon2 deriving (Generic, Show)
 instance FromJSON ConfJSON
 
-testReadConfJSON :: ConfJSON -> Bool -> IO L.ByteString
+testReadConfJSON :: ConfJSON -> Bool -> IO LazyByteString
 testReadConfJSON = ignitionService testRead
 ngxExportSimpleServiceTypedAsJSON 'testReadConfJSON ''ConfJSON
     SingleShotService
 
-testReadIntHandler :: ByteString -> L.ByteString
+testReadIntHandler :: ByteString -> LazyByteString
 testReadIntHandler = showAsLazyByteString .
     readFromByteString @Int
 ngxExportYY 'testReadIntHandler
 
-testReadConfHandler :: ByteString -> L.ByteString
+testReadConfHandler :: ByteString -> LazyByteString
 testReadConfHandler = showAsLazyByteString .
     readFromByteString @Conf
 ngxExportYY 'testReadConfHandler
 
-testReadConfJSONHandler :: ByteString -> IO L.ByteString
+testReadConfJSONHandler :: ByteString -> IO LazyByteString
 testReadConfJSONHandler = return . showAsLazyByteString .
     readFromByteStringAsJSON @ConfJSON
 ngxExportAsyncIOYY 'testReadConfJSONHandler
 
-testReadConfWithRPtrHandler :: ByteString -> L.ByteString
+testReadConfWithRPtrHandler :: ByteString -> LazyByteString
 testReadConfWithRPtrHandler = showAsLazyByteString .
     readFromByteStringWithRPtr @Conf
 ngxExportYY 'testReadConfWithRPtrHandler
 
-testReadConfWithRPtrJSONHandler :: ByteString -> L.ByteString
+testReadConfWithRPtrJSONHandler :: ByteString -> LazyByteString
 testReadConfWithRPtrJSONHandler = showAsLazyByteString .
     readFromByteStringWithRPtrAsJSON @ConfJSON
 ngxExportYY 'testReadConfWithRPtrJSONHandler
 
-testLoadConf :: Conf -> Bool -> IO L.ByteString
+testLoadConf :: Conf -> Bool -> IO LazyByteString
 testLoadConf = voidService
 
 ngxExportSimpleServiceTyped 'testLoadConf ''Conf restartPromptly
 
-testLoadConfStorage :: ByteString -> IO L.ByteString
+testLoadConfStorage :: ByteString -> IO LazyByteString
 testLoadConfStorage = const $
     showAsLazyByteString <$> readIORef storage_Conf_testLoadConf
 ngxExportIOYY 'testLoadConfStorage
